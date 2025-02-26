217a67dc9e1f4a4f942c8c7bed8daa63053435ab
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-02-26 11:25:02 +0900

    study11

---
+++ study11/study11/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*int[] num = new int[3];
+
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(num[i]);
+            }*/
+
+			/*int[] num = new int[3];
+
+			num[0] = 10;
+			num[1] = 20;
+			num[2] = 30;
+
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine(num[i]);
+			}*/
+
+			/*int[] iKor = new int[3];
+			int[] iEng = new int[3];
+			int[] iMath = new int[3];
+
+			int[] sum = new int[3];
+			float[] aver = new float[3];
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine("학생 성적입력하세요 : ");
+				Console.Write("국어 : ");
+				iKor[i] = int.Parse(Console.ReadLine());
+				Console.Write("영어 : ");
+				iEng[i] = int.Parse(Console.ReadLine());
+				Console.Write("수학 : ");
+				iMath[i] = int.Parse(Console.ReadLine());
+
+
+				sum[i] = iKor[i] + iEng[i] + iMath[i];
+
+				aver[i] = (float)sum[i] / 3;
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine((i + 1) + "번학생");
+				Console.WriteLine($"국어 : {iKor[i]} 영어 : {iEng[i]} 수학 : {iMath[i]} ");
+				Console.WriteLine("총점 : " + sum[i]);
+				Console.WriteLine("평균 : " + aver[i].ToString("F2"));
+			}*/
+
+			/*int[] score = new int[3];
+
+			score[0] = 90;
+			score[1] = 85;
+			score[2] = 88;
+
+			for(int i = 0; i < score.Length; i++)
+			{
+				Console.WriteLine($"점수 {i + 1} : {score[i]}");
+			}*/
+
+			/*double value = 123.456789;
+
+			Console.WriteLine(value.ToString("F2"));
+			Console.WriteLine($"소수점 둘째자리 : {value:F2}");
+			Console.WriteLine(string.Format("소수점 둘째자리 : {0:F2}", value));
+		
+			Console.WriteLine(value.ToString("F0"));*/
+
+			/*double value = 123124124.12323;
+
+			Console.WriteLine(value.ToString("N2"));*/
+
+			/*int[,] matrix = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+
+
+			for (int i = 0; i < 2; i++)
+			{
+
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write($"{matrix[i, j]}");
+				}
+				Console.WriteLine();
+			}*/
+
+			/*int[][] matrix = new int[2][];
+
+			matrix[0] = new int[3];
+			matrix[1] = new int[3];
+
+			matrix[0][0] = 1;
+			matrix[0][1] = 2;
+			matrix[0][2] = 3;
+
+			matrix[1][0] = 4;
+			matrix[1][1] = 5;
+			matrix[1][2] = 6;
+
+			for (int i = 0; i < matrix.Length; i++)
+			{
+				for (int j = 0; j < matrix[i].Length; j++)
+				{
+					Console.Write(matrix[i][j] + " ");
+				}
+				Console.WriteLine();
+
+			}*/
+
+			/*Console.WriteLine("가변 배열");
+			int[][] jaggedArray = new int[3][];
+
+			jaggedArray[0] = new int[] { 1, 2 };
+			jaggedArray[1] = new int[] { 3, 4, 5 };
+			jaggedArray[2] = new int[] { 6 };
+
+
+			for (int i = 0; i < jaggedArray.Length; i++)
+			{
+				for (int j = 0; j < jaggedArray[i].Length; j++)
+				{
+					Console.Write($"{jaggedArray[i][j]}");
+				}
+				Console.WriteLine();
+			}
+
+			Console.WriteLine("var 키워드 사용");
+			var numbers = new[] { 1, 2, 3, 4, 5 };
+			Console.WriteLine($"배열 타입: {numbers.GetType()}");*/
+
+
+		}
+	}
+}
--- 콘솔좌표배우기/콘솔좌표배우기/Program.cs
+++ 콘솔좌표배우기/콘솔좌표배우기/Program.cs
@@ -1,15 +1,227 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace 콘솔좌표배우기
 {
     class Program
     {
+        static void EdgeDraw()
+        {
+			char[] arrChar = new char[80];
+
+			Console.Clear();
+
+			for (int i = 0; i < 25; i++)
+			{
+				switch (i)
+				{
+					case 0:
+						for (int j = 0; j < 80; j++)
+						{
+							switch (j)
+							{
+								case 0:
+									arrChar[j] = '┏';
+									break;
+								case 79:
+									arrChar[j] = '┓';
+									break;
+								default:
+									arrChar[j] = '━';
+									break;
+							}
+						}
+						Console.SetCursorPosition(0, i);
+						Console.Write(arrChar);
+						break;
+					case 24:
+						for (int j = 0; j < 80; j++)
+						{
+							switch (j)
+							{
+								case 0:
+									arrChar[j] = '┗';
+									break;
+								case 79:
+									arrChar[j] = '┛';
+									break;
+								default:
+									arrChar[j] = '━';
+									break;
+							}
+						}
+						Console.SetCursorPosition(0, i);
+						Console.Write(arrChar);
+						break;
+					default:
+						for (int j = 0; j < 80; j++)
+						{
+							switch (j)
+							{
+								case 0:
+									arrChar[j] = '┃';
+									break;
+								case 79:
+									arrChar[j] = '┃';
+									break;
+								default:
+									arrChar[j] = ' ';
+									break;
+							}
+						}
+						Console.SetCursorPosition(0, i);
+						Console.Write(arrChar);
+						break;
+				}
+			}
+		}
+
+		static void ConsoleWriteXY(int a, int b, string str)
+		{
+			Console.SetCursorPosition(a, b);
+			Console.Write(str);
+		}
+
+		static void DungeonExploring()
+		{
+			EdgeDraw();
+			ConsoleWriteXY(35, 12, "던전 탐험중");
+			Thread.Sleep(500);
+			EdgeDraw();
+			ConsoleWriteXY(35, 12, "던전 탐험중.");
+			Thread.Sleep(500);
+			EdgeDraw();
+			ConsoleWriteXY(35, 12, "던전 탐험중..");
+			Thread.Sleep(500);
+			EdgeDraw();
+			ConsoleWriteXY(35, 12, "던전 탐험중...");
+			Thread.Sleep(500);
+		}
+
         static void Main(string[] args)
         {
+            //콘솔 창 크기 설정
+            Console.SetWindowSize(80, 25);
+
+            //콘솔 버퍼 크기도 설정(스크롤없이 고정된 창 유지)
+            Console.SetBufferSize(80, 25);
+            Console.CursorVisible = false;
+
+			Random rand = new Random();
+
+			const int MAXHP = 100;
+
+			int att = 70;
+			int addAtt = 0;
+			int monsterAtt = 0;
+			int currentHp = 100;
+			int damage = 0;
+			int gold = 100;
+			int reward = 0;
+			int choice = 0;
+			int eventNum = 0;
+
+			bool isLoop = true;
+
+			EdgeDraw();
+			ConsoleWriteXY(35, 12, "RPG TEST");
+			Thread.Sleep(3000);
+
+			while(isLoop)
+			{
+				EdgeDraw();
+				ConsoleWriteXY(30, 5, "   캐릭터 상태   ");
+				ConsoleWriteXY(30, 6, "=================");
+				ConsoleWriteXY(30, 7, "    공격력 : " + att);
+				ConsoleWriteXY(30, 8, "    체력   : " + currentHp);
+				ConsoleWriteXY(30, 9, "    소지금 : " + gold);
+				ConsoleWriteXY(30, 10, "=================");
+				ConsoleWriteXY(30, 11, "      메뉴       ");
+				ConsoleWriteXY(30, 12, "=================");
+				ConsoleWriteXY(30, 13, "    1.던전 탐험  ");
+				ConsoleWriteXY(30, 14, "    2.장비 뽑기  ");
+				ConsoleWriteXY(30, 15, "    3.캐릭터 훈련  ");
+				ConsoleWriteXY(30, 16, "    4.휴식하기  ");
+				ConsoleWriteXY(30, 17, "    5.게임 종료하기  ");
+				ConsoleWriteXY(25, 19, "원하는 메뉴의 번호를 입력 :");
+
+				choice = int.Parse(Console.ReadLine());
+				Thread.Sleep(1000);
+
+				switch(choice)
+				{
+					case 1:
+						DungeonExploring();
+
+						eventNum = rand.Next(1, 101);
+
+						if (eventNum < 31)
+						{
+							EdgeDraw();
+							ConsoleWriteXY(25, 12, "몬스터와 조우하였다");
+							monsterAtt = rand.Next(50, 201);
+
+							if (att < monsterAtt)
+							{
+								damage = monsterAtt / 4;
+								currentHp -= damage;
+								ConsoleWriteXY(25, 13, "몬스터가 나보다 강했다");
+								ConsoleWriteXY(25, 14, $"나는 {damage}에 데미지를 입었다");
+
+								if (currentHp < 1)
+								{
+									EdgeDraw();
+									ConsoleWriteXY(25, 12, "나의 체력이 바닥나 쓰러졌다...");
+									isLoop = false;
+								}
+							}
+							else
+							{
+								reward = rand.Next(100, 251);
+								gold += reward;
+								ConsoleWriteXY(25, 13, "몬스터를 손쉽게 처치하였다");
+								ConsoleWriteXY(25, 14, $"몬스터를 처치하여 {reward}골드를 획득히였다");
+							}
+						}
+						else if (eventNum < 81)
+						{
+							reward = rand.Next(50, 321);
+							gold += reward;
+							EdgeDraw();
+							ConsoleWriteXY(25, 12, "보물상자를 발견하였다");
+							ConsoleWriteXY(25, 13, $"{reward}골드를 획득히였다");
+						}
+						else
+						{
+							damage = 10;
+							currentHp -= damage;
+							ConsoleWriteXY(25, 12, "함정에 빠졌다!");
+							ConsoleWriteXY(25, 12, $"{damage}에 체력을 잃었다...!");
+							EdgeDraw();
+						}
+						Thread.Sleep(3000);
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+					default:
+						break;
+				}
+			}
+
+			ConsoleWriteXY(35, 12, "GAME OVER!!");  
+            
         }
     }
 }
