c0675eb1d80c0ca2030eb2f082fb8883715f893c
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-02-27 13:25:08 +0900

    struct1

---
+++ study17/study17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study17
+{
+    class Program
+    {
+
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"좌표 :  {X} , {Y}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 15);
+
+            p1.Print();
+
+        }
+    }
+}
