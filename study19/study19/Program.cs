332fbdeaa1768540b6d9401b525e92d283e3ef84
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-02-28 09:29:43 +0900

    study19

---
+++ study19/study19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine($"Current Date and Time : {now}");
+
+            TimeSpan duration = new TimeSpan(1, 30, 0);// 1시간 30분 (시간, 분, 초)
+
+            Console.WriteLine($"Duration : {duration}");
+
+        }
+    }
+}
