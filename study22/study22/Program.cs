add2b450515e3765afa062555d8d18cdf33faa39
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-04 10:04:57 +0900

    study22

---
+++ study22/study22/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace study22
+{
+    /*public class Player
+    {
+        public string Name { get; set; }
+        public int Socre { get; set; }
+    }
+
+    public class Warrior : Player
+    {
+        public int Strength { get; set; }
+    }
+
+    public class Enemy : IAttackable, IMove
+    {
+        public void Attack() { }
+        public void Move() { }
+    }
+
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+    }*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("프로그램 종료");
+
+            string path = Environment.GetEnvironmentVariable("PATH");
+            Console.WriteLine($"PATH : {path}");
+
+            Environment.Exit(0);*/
+
+            /*Random random = new Random();
+
+            int randomNumber = random.Next(1, 101);
+            Console.WriteLine("램덤 숫자 : " + randomNumber);*/
+
+            /*Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for(int i = 0; i < 1000000; i++)
+            {
+                Thread.Sleep(1);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"for 시간 : {stopwatch.ElapsedMilliseconds}ms");*/
+
+            /*string input = "Hello, my phone number is 010-1234-5678.";
+            string patter = @"\d{3}-\d{4}-\d{4}";
+
+            bool isMatch = Regex.IsMatch(input, patter);
+            Console.WriteLine($"전화번호가 존해하는가? {isMatch}");*/
+
+        }
+    }
+}
