f28541c1c325ed07919aa7de863623a358f3b938
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-05 13:19:20 +0900

    study29

---
+++ study29/study29/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study29
+{
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("동물이 소리를 냅니다");
+        }
+    }
+
+    class Dog : Animal
+    {
+		public override void Speak()
+		{
+            Console.WriteLine($"{Name}이(가) 멍멍 짖습니다");
+		}
+	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+
+            myAnimal.Name = "일반동물";
+            myAnimal.Speak();
+
+            Dog myDog = new Dog();
+
+            myDog.Name = "바둑이";
+            myDog.Speak();
+
+        }
+    }
+}
