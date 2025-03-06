fd8fa99be14e88f0f1396ecc289505e588b3dfab
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 15:21:28 +0900

    study43

---
+++ study43/study43/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study43
+{
+    interface IAinimal
+    {
+        void MakeSound();
+    }
+
+	class Dog : IAinimal
+	{
+		public void MakeSound()
+		{
+            Console.WriteLine("멍멍!");
+		}
+	}
+
+	class Cat : IAinimal
+	{
+		public void MakeSound()
+		{
+			Console.WriteLine("야옹!");
+		}
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			IAinimal dog = new Dog();
+			IAinimal cat = new Cat();
+
+			dog.MakeSound();
+			cat.MakeSound();
+
+        }
+    }
+}
