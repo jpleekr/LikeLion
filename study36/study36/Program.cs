39cda706432e93344b262983eb17654d49bd7a0e
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 09:34:58 +0900

    study36

---
+++ study36/study36/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study36
+{
+    class Person
+    {
+        private string Name ="홍길동";
+
+        public void SetName(string n)
+        {
+            Name = n;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            Console.WriteLine(p.GetName());
+            p.SetName("Bob");
+            Console.WriteLine(p.GetName());
+
+        }
+    }
+}
