4f2503b3a65648ab355125798b64f5e5d15262b8
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 09:22:40 +0900

    study34

---
+++ Study35/Study35/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hello
+
+namespace Hello
+{
+    class Say
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("안녕하세요!");
+        }
+    }
+}
+
+namespace Study35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Say say = new Say();
+
+            say.SayHello();
+
+        }
+    }
+}
