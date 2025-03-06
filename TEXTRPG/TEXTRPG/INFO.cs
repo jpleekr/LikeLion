7d725b9711f0fda1e2233dac7dc3bec3d55984b0
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 15:11:47 +0900

    textrpg

---
+++ TEXTRPG/TEXTRPG/INFO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXTRPG
+{
+    class INFO
+    {
+        public string strName { get; set; }
+        public int iAttack { get; set; }
+		public int iHP { get; set; }
+
+	}
+}
---
+++ TEXTRPG/TEXTRPG/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXTRPG
+{
+    class Player
+    {
+        public INFO m_tInfo;
+
+        public void SetDamage(int iAttack) { m_tInfo.iHP -= iAttack; }
+
+        public INFO GetInfo() { return m_tInfo; }
+
+        public void SetHp(int hp) { m_tInfo.iHP = hp; }
+
+        public void SelectJob()
+        {
+            m_tInfo = new INFO();
+
+            Console.WriteLine("직업을 선택하세요 (1.기사 2.마법사 3.도둑) : ");
+            int iInput = 0;
+
+            iInput = int.Parse(Console.ReadLine());
+
+            switch(iInput)
+            {
+                case 1:
+                    m_tInfo.strName = "기사";
+                    m_tInfo.iHP = 100;
+                    m_tInfo.iAttack = 10;
+                    break;
+				case 2:
+					m_tInfo.strName = "마법사";
+					m_tInfo.iHP = 90;
+					m_tInfo.iAttack = 15;
+					break;
+				case 3:
+					m_tInfo.strName = "도둑";
+					m_tInfo.iHP = 85;
+					m_tInfo.iAttack = 13;
+					break;
+			}
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("=================");
+            Console.WriteLine("직업 이름 : " + m_tInfo.strName);
+            Console.WriteLine("체력 : " + m_tInfo.iHP + "\t공격력 : " + m_tInfo.iAttack);
+
+        }
+
+        public Player() { }
+
+        ~Player() { }
+
+    }
+}
