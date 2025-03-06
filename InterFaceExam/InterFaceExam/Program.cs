595106161ee8c38571b8c94e5b257c287cbd17af
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 15:31:40 +0900

    interface1

---
+++ InterFaceExam/InterFaceExam/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceExam
+{
+	interface IAnimal
+	{
+		void Speak();
+	}
+
+	class Dog : IAnimal
+	{
+		public void Speak()
+		{
+			Console.WriteLine("멍멍");
+		}
+	}
+
+	class Cat : IAnimal
+	{
+		public void Speak()
+		{
+			Console.WriteLine("야옹~");
+		}
+	}
+
+	class AnimalTrainer
+	{
+		public void Train(IAnimal animal)
+		{
+			Console.Write("동물이 소리를 냅니다.");
+			animal.Speak();
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AnimalTrainer trainer = new AnimalTrainer();
+
+			IAnimal myDog = new Dog();
+			IAnimal myCat = new Cat();
+
+			trainer.Train(myDog);
+			trainer.Train(myCat);
+
+		}
+	}
+}
