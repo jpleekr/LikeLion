b4a5a09907309b91695071db7efa1f4529b264cf
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 10:33:15 +0900

    study38

---
+++ study38/study38/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study38
+{
+    /*class MyResource
+    {
+        ~MyResource()
+        {
+            Console.WriteLine("삭제될때 호출");
+        }
+    }*/
+
+    class Program
+    {
+        static void Increase(ref int x)
+        {
+            x++;
+        }
+
+        static void OutFunc(int a, int b, out int x, out int y)
+        {
+            x = a;
+            y = b;
+        }
+
+
+        static void Main(string[] args)
+        {
+            /*MyResource r = new MyResource();*/
+            int a = 10;
+            int b = 20;
+            int x, y;
+
+            Increase(ref a);
+
+            Console.WriteLine(a);
+
+            OutFunc(a, b,out x,out y);
+
+            Console.WriteLine($"{x}, {y}");
+
+        }
+    }
+}
