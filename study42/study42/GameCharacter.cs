636b9e459265e39e5f29d896b60ba219994745ed
jpleekr <109937740+jpleekr@users.noreply.github.com>
2025-03-06 13:04:51 +0900

    study42

---
+++ study42/study42/GameCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace study42
+{
+    public abstract class GameCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        protected GameCharacter(string name, int health, int attack, int defense)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+            Defense = defense;
+        }
+        public abstract void BasicAttack(GameCharacter target);
+
+        public abstract void SpecialAttack(GameCharacter target);
+
+        public void TakeDamage(int damage)
+        {
+            int actualDamage = Math.Max(1, damage - Defense);
+
+            Health = Math.Max(0, Health - actualDamage);
+
+            Console.WriteLine($"{Name}이 {actualDamage}의 피해를 받았습니다. 남은 체력 : {Health}");
+        }
+
+    }
+}
